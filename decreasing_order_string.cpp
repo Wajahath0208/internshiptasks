b2ab53b7220f01d0e833acac2d716a281977e175
Wajahath0208 <85498663+Wajahath0208@users.noreply.github.com>
2022-11-10 21:02:31 +0530

    Update decreasing_order_string.cpp

--- decreasing_order_string.cpp
+++ decreasing_order_string.cpp
@@ -1,3 +1,60 @@
+// Decreasing Order Strings
+// Sorting numbers is something we are very much comfortable with. Let's get comfortable with sorting strings as well. The task is simple, given a string 
+// S
+//  you have to sort the characters of the string in lexicographically-decreasing order.
+
+// Note - Do not use inbuilt sorting function.
+
+// Input format
+// The first line contains an integer 
+// T
+// , denoting the number of test cases.
+// Each test case consists of a string 
+// S
+//  containing only lowercase characters.
+
+// Output format
+// For each test case on a new line, print the string sorted in lexicographically-decreasing order.
+
+// Constraints
+// 1
+// <=
+// T
+// <=
+// 5
+
+// 1
+// <=
+// |
+// S
+// |
+// <=
+// 10
+// 6
+// , where 
+// |
+// S
+// |
+//  denotes length of string 
+// S
+// .
+
+// Time limit
+// 1
+//  second
+
+// Example
+// Input
+// 2
+
+// prepbytes
+// algorithm
+
+// Output
+// ytsrppeeb
+// tromlihga
+
+
 #include <bits/stdc++.h>
   using namespace std;
   
